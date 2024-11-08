bee177c71e0ed8b596da672c4cb86c4bc7ff48ca
Kusalana Buddhadasa <kslnrnmyr@gmail.com>
2024-11-08 22:37:09 +0530

    Completed week 3,2 and 4 challenges

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Button from "./Button";
+import { Button } from "./components/ui/button";
 import Hero from "./Hero";
 import Navigation from "./Navigation";
+import { Slider } from "@/components/ui/slider"
+import { Switch } from "@/components/ui/switch"
+import { Badge } from "@/components/ui/badge"
+
 
 function App() {
-  const name = '';
+  const name = 'Kusalana';
   const cartCount = 2;
 
   return (
@@ -11,7 +15,17 @@ function App() {
       <Navigation name={name} cartCount={cartCount} />
       <Hero />
       <div className="p-4">
-        <Button>Buy Now</Button>
+        <Button variant="destructive">Buy Now</Button>
+        <div className="w-48 h-6 bg-slate-200 p-4 flex items-center rounded-xl mt-5">
+        <Slider defaultValue={[10]} max={100} step={1}  />
+        </div>
+        <div className=" flex items-center rounded-xl mt-5">
+        <Switch/>
+        </div>
+        <div className="mt-5">
+        <Badge variant="outline">Badge</Badge>
+        </div>
+
       </div>
     </div>
   );
--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,5 +1,5 @@
 function Greeting() {
-  const name = "Manupa";
+  const name = "Kusalana";
 
   return (
     <div>
