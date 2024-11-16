5e8121ffb64e4e060ef2252f2187fb0e8be24960
Kusalana Buddhadasa <kslnrnmyr@gmail.com>
2024-11-16 08:10:06 +0530

    Completed week 5 challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { Button } from "./components/ui/button";
 import Hero from "./Hero";
 import Navigation from "./Navigation";
-import { Slider } from "@/components/ui/slider"
-import { Switch } from "@/components/ui/switch"
-import { Badge } from "@/components/ui/badge"
+import Products from "./Products";
+
+
 
 
 function App() {
@@ -14,19 +13,9 @@ function App() {
     <div>
       <Navigation name={name} cartCount={cartCount} />
       <Hero />
-      <div className="p-4">
-        <Button variant="destructive">Buy Now</Button>
-        <div className="w-48 h-6 bg-slate-200 p-4 flex items-center rounded-xl mt-5">
-        <Slider defaultValue={[10]} max={100} step={1}  />
-        </div>
-        <div className=" flex items-center rounded-xl mt-5">
-        <Switch/>
-        </div>
-        <div className="mt-5">
-        <Badge variant="outline">Badge</Badge>
-        </div>
+      <Products />
 
-      </div>
+      
     </div>
   );
 }
